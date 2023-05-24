300205887fb718ea3dd5c2a61dc6b73b23031f81
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-05-24 12:11:59 +0300

    add registr form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,54 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  function handleSubmit(event) {
+    console.log("hi");
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/register", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="auth">
       <h1>Register</h1>
-      <form>
-        <input required type="text" placeholder="login" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Register</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+          name="name"
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+        />
+        <button type="submit">Login</button>
         <p>this is error</p>
         <span>
-          Do you have an account?<Link to={"/login"}>Auth</Link>
+          Don't you have an account?<Link to={"/login"}>Register</Link>
         </span>
       </form>
     </div>
