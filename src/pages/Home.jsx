efc3f80c5173ba579360273d006ceefe7f7e66de
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-05-28 20:30:20 +0300

    add file home.scss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import client from "../contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import "../style/Home.scss";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
